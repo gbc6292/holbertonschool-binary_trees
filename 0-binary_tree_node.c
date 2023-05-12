3f97adeca2dfac7e04a982d097116918a2579d75
gbc6292 <6292@holbertonstudents.com>
2023-05-12 14:11:45 +0000

    Adding a new node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_node - creates a binary tree node
+ *
+ * @parent: Pointer to the parent node
+ * @value: Value to be inserted in the new node. 
+ * Return: A pointer to new node or NULL on failure.
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
+	return (new);
+}
