04ea3a69db1bb709a6403cdc6ea5893b3f26aec4
gbc6292 <6292@holbertonstudents.com>
2023-05-15 17:12:52 +0000

    Correcting output check

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,7 +10,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t left_height, right_height;
 	
 	if (tree == NULL)
-		return -1;
+		return 0;
 	
 	if (tree->left == NULL && tree->right == NULL)
 		return 0;
